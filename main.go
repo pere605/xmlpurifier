b9cd90e280b2980044ed0ed58b0c72af3345f19e
Piotr Leśniowski <piotr.lesniowski@innogames.com>
2019-09-09 22:03:42 +0200

    initial commit

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"regexp"
+	"strings"
+)
+
+func main() {
+	sourceFile := os.Args[1]
+	file, error := ioutil.ReadFile(sourceFile)
+
+	if error != nil {
+		panic("Could not read the source file")
+	}
+
+	cleanedContents := removeEmptyElements(string(file))
+
+	writingError := ioutil.WriteFile(
+		getDestinationFile(),
+		[]byte(strings.TrimSpace(cleanedContents)),
+		0644)
+
+	if writingError != nil {
+		panic("Could not write to the destination file")
+	}
+}
+
+func getDestinationFile() string {
+	if len(os.Args) == 3 {
+		return os.Args[2]
+	}
+
+	return os.Args[1]
+}
+
+func removeEmptyElements(contents string) string {
+	re := regexp.MustCompile(`\r\n[ \t]*<.+[^\/]\/>`)
+
+	return re.ReplaceAllString(contents, "")
+}
